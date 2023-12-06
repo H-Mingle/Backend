f7d16f454cfed20fb3014e965cc9d0a5f5b50cca
yukong <kyb062666@gmail.com>
2023-12-06 22:22:52 +0900

    feat: 발급한 토큰 저장

--- H-Mingle/src/main/java/com/hyundai/hmingle/service/OauthService.java
+++ H-Mingle/src/main/java/com/hyundai/hmingle/service/OauthService.java
@@ -6,6 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.hyundai.hmingle.controller.dto.response.OauthLoginResponse;
 import com.hyundai.hmingle.controller.dto.response.OauthLoginUrlResponse;
+import com.hyundai.hmingle.domain.member.Member;
+import com.hyundai.hmingle.domain.member.Token;
+import com.hyundai.hmingle.mapper.TokenMapper;
 import com.hyundai.hmingle.support.JwtTokenProvider;
 import com.hyundai.hmingle.support.OauthConnector;
 import com.hyundai.hmingle.support.OauthProvider;
@@ -22,6 +25,7 @@ public class OauthService {
 	private final OauthProvider googleOauthProvider;
 	private final OauthConnector googleOauthConnector;
 	private final JwtTokenProvider jwtTokenProvider;
+	private final TokenMapper tokenMapper;
 
 	@Transactional(readOnly = true)
 	public OauthLoginUrlResponse generateLoginUrl(String redirectUrl) {
@@ -32,9 +36,12 @@ public class OauthService {
 	public OauthLoginResponse login(String redirectUrl, String authorizationCode) {
 		ResponseEntity<GoogleUserResponse> response = googleOauthConnector.requestUserInfo(redirectUrl, authorizationCode);
 
-		String accessToken = jwtTokenProvider.createAccessToken("1L");
-		String refreshToken = jwtTokenProvider.createRefreshToken("1L");
+		Long memberId = 1L;
+		String accessToken = jwtTokenProvider.createAccessToken(String.valueOf(memberId));
+		String refreshToken = jwtTokenProvider.createRefreshToken(String.valueOf(memberId));
 
+		Member member = Member.saved(memberId, "email", "nickname", "introduction");
+		tokenMapper.save(new Token(member, accessToken, refreshToken));
 		return new OauthLoginResponse(accessToken, refreshToken);
 	}
 }
