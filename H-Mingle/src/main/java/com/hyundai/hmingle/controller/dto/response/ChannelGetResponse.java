377cf92c8cd3ed38313254b05b664bec2cd050d6
이혜연 <tjsalsu@naver.com>
2023-12-06 17:53:27 +0900

    feat: 채널별 게시물수 조회

--- H-Mingle/src/main/java/com/hyundai/hmingle/controller/dto/response/ChannelGetResponse.java
+++ H-Mingle/src/main/java/com/hyundai/hmingle/controller/dto/response/ChannelGetResponse.java
@@ -14,6 +14,6 @@ public class ChannelGetResponse {
 	private String businessHours;
 	private String phoneNumber;
 	private Integer count;
-	private String recent;//최근에 수정된 게시물의 날짜 -> 몇 분 전  
+	private String recent;
 	
 }
