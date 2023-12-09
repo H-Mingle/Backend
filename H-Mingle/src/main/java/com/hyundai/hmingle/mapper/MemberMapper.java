79e307d6f50698c0aa68837a0d3101dcf3eeb6b2
yukong <kyb062666@gmail.com>
2023-12-09 22:40:45 +0900

    refactor: mybatis 어노테이션에서 xml 파일로 변경

--- H-Mingle/src/main/java/com/hyundai/hmingle/mapper/MemberMapper.java
+++ H-Mingle/src/main/java/com/hyundai/hmingle/mapper/MemberMapper.java
@@ -2,19 +2,11 @@ package com.hyundai.hmingle.mapper;
 
 import java.util.Optional;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-
 import com.hyundai.hmingle.domain.member.Member;
 
 public interface MemberMapper {
 
-	@Insert("insert into member(id, email, nickname, introduction, image_url)\n"
-		+ "        values (SEQ_MEMBER_ID.nextval, #{email}, #{nickname}, #{introduction}, #{imageUrl})")
 	void save(Member member);
 
-	@Select("select id, email, nickname, introduction, image_url\n"
-		+ "        from member\n"
-		+ "        where email = #{email}")
 	Optional<Member> findByEmail(String email);
 }
--- H-Mingle/src/main/java/com/hyundai/hmingle/mapper/TokenMapper.java
+++ H-Mingle/src/main/java/com/hyundai/hmingle/mapper/TokenMapper.java
@@ -2,26 +2,13 @@ package com.hyundai.hmingle.mapper;
 
 import java.util.Optional;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-
 import com.hyundai.hmingle.domain.member.Token;
 
 public interface TokenMapper {
 
-	@Insert("insert into token (id, member_id, access_token, refresh_token)\n"
-		+ "        values (SEQ_TOKEN_ID.nextval, #{member.id}, #{accessToken}, #{refreshToken})")
 	void save(Token token);
 
-	@Update("update token\n"
-		+ "        set access_token = #{accessToken},\n"
-		+ "            refresh_token = #{refreshToken}\n"
-		+ "        where member_id = #{member.id}")
 	void update(Token token);
 
-	@Select("select id, member_id, access_token, refresh_token\n"
-		+ "        from token\n"
-		+ "        where member_id = #{memberId}")
 	Optional<Token> findByMemberId(Long memberId);
 }
