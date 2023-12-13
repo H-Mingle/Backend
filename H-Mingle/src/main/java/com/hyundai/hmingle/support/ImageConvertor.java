b351154f4b1049d4cebb8e3cdd4b04d827541794
yukong <kyb062666@gmail.com>
2023-12-13 22:26:45 +0900

    feat: 파일이 없을 경우 null 값 반환

--- H-Mingle/src/main/java/com/hyundai/hmingle/support/ImageConvertor.java
+++ H-Mingle/src/main/java/com/hyundai/hmingle/support/ImageConvertor.java
@@ -1,6 +1,7 @@
 package com.hyundai.hmingle.support;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -15,6 +16,8 @@ public class ImageConvertor {
 	public byte[] convertPath(String path) {
 		try (InputStream imageStream = new FileInputStream(path)) {
 			return IOUtils.toByteArray(imageStream);
+		} catch (FileNotFoundException e) {
+			return null;
 		} catch (IOException e) {
 			throw new RuntimeException("이미지를 불러오는데 실패히였습니다.");
 		}
